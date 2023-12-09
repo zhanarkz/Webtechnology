ea404b3900c1dfc897044e7487da59b43ea5ac93
Zhanar <zhanarkz@gmail.com>
2023-12-09 23:31:55 +0600

    HW3

---
+++ HW3/hw3.js
@@ -0,0 +1,6 @@
+// Создать страницу, которая спрашивает имя у пользователя и выводит его с помощью функции.
+function greeting (name) {
+    alert(`Hello, ${name}`)
+}
+
+greeting(prompt('Type your name, please '))
